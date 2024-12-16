ef76a55c070a062460e054bc1e5f75f89c3bd7aa
Feras3245 <fhennidi@gmail.com>
2024-12-16 23:40:22 +0100

    sidemenu fully integrated

--- assets/app.js
+++
@@ -1,7 +0,0 @@
-// Toggle Nav-Sidebar
-$( function() {
-    // Set effect from select menu value
-    $( "#nav-sidebar-toggler" ).on( "click", function() {
-        $("#nav-sidebar").animate({width: 'toggle'}, 200);
-    });
-});
