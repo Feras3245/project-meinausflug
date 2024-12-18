e7a533ecba9dc693b145a993b5017c07265989e4
Feras3245 <fhennidi@gmail.com>
2024-12-18 16:15:54 +0100

    destination card view complete

--- src/Controllers/MainController.php
+++ src/Controllers/MainController.php
@@ -1,75 +1,29 @@
 <?php
-
+// src/Controller/MainController.php
 namespace App\Controllers;
 
+use App\Entity\Destination;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Contracts\Translation\TranslatorInterface;
-
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Finder\Finder;
 
-class MainController extends AbstractController {
-
-    // #[Route('/')]
-    // public function index(TranslatorInterface $translator): Response {
-    //     // return $this->render('main.html.twig');
-    //     ob_start();
-    //     phpinfo();
-    //     $phpinfo = ob_get_clean();
-
-    //     return $this->render('main.html.twig', array('phpinfo' => $phpinfo));
-    // }
-
-    // #[Route('/', name: 'index')]
-    // public function test(): Response
-    // {
-    //     $baseDir = $this->getParameter('kernel.project_dir') . '/assets/images/header';
-    //     $headerImages = [];
-
-    //     // Use Symfony Finder to read directory structure
-    //     $finder = new Finder();
-    //     $finder->directories()->in($baseDir);
-
-    //     foreach ($finder as $dir) {
-    //         $location = $dir->getFilename(); // Get subfolder name
-    //         $headerImages[$location] = [];
-
-    //         // Find image files in the subfolder
-    //         $images = new Finder();
-    //         $images->files()->in($dir->getRealPath())->name('/\.(jpg|jpeg|png|gif)$/i');
-
-    //         foreach ($images as $image) {
-    //             $headerImages[$location][] = '/images/header/' . $location . '/' . $image->getFilename();
-    //         }
-    //     }
-
-    //     $baseDir = $this->getParameter('kernel.project_dir') . '/assets/images/destinations';
-
-    //     $destImages = [];
-    //     // Populate $destImages with the image files found in '/assets/images/destinations/{destination_country}'
-
-    //     return $this->render('main.html.twig', [
-    //         'header' => $headerImages,
-    //         'destinationImages' => $destImages,
-    //     ]);
-    // }
-
+class MainController extends AbstractController
+{
     #[Route('/', name: 'index')]
-    public function test(): Response
+    public function index(): Response
     {
+        // Keep the existing header images logic
         $baseDir = $this->getParameter('kernel.project_dir') . '/assets/images/header';
         $headerImages = [];
 
-        // Use Symfony Finder to read directory structure
         $finder = new Finder();
         $finder->directories()->in($baseDir);
 
         foreach ($finder as $dir) {
-            $location = $dir->getFilename(); // Get subfolder name
+            $location = $dir->getFilename();
             $headerImages[$location] = [];
 
-            // Find image files in the subfolder
             $images = new Finder();
             $images->files()->in($dir->getRealPath())->name('/\.(jpg|jpeg|png|gif)$/i');
 
@@ -78,7 +32,7 @@ class MainController extends AbstractController {
             }
         }
 
-        // Load "UAE" destination images
+        // Load UAE destination images
         $uaeDir = $this->getParameter('kernel.project_dir') . '/assets/images/destinations/UAE';
         $destImages = [];
 
@@ -91,9 +45,27 @@ class MainController extends AbstractController {
             }
         }
 
+        // Create the UAE Destination object
+        $destinationUAE = new Destination(
+            destID: 1, // Unique identifier for the destination
+            city: 'Dubai',
+            country: 'UAE',
+            subtitle: 'From desert adventures to thrilling waterparks, Dubai has it all',
+            destImages: $destImages,
+            reviewsCount: 15,
+            avgScore: 4.7,
+            description: "See the city from above at Skydive Dubai, or brave the edge walk with family and friends to enjoy spectacular views of the Burj Khalifa and the Dubai Fountain. You can also visit an indoor tropical forest with over 3,000 plants and animals at The Green Planet.
+
+Dubai's skyline is dotted with architectural marvels and surprising feats of engineering. Start your holiday with a visit to the top of Burj Khalifa, the world's tallest tower, or fast forward in time at the Museum of the Future.
+
+One of the best places to start your Dubai experience is at Dubai Museum in the historic area by the Dubai Creek. Discover Dubai's pearl diving roots and take a traditional boat ride between the textile, spice and gold souks.
+ "
+        );
+
+        // Render the template with both header images and destination object
         return $this->render('main.html.twig', [
             'header' => $headerImages,
-            'destinationImages' => $destImages,
+            'destination' => $destinationUAE, // Pass the destination object
         ]);
     }
 }
--- src/Entity/Destination.php
+++ src/Entity/Destination.php
@@ -1,149 +1,45 @@
 <?php
-
+// src/Model/DestinationUAE.php
 namespace App\Entity;
 
-use App\Repository\DestinationRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\DBAL\Types\Types;
-use Doctrine\ORM\Mapping as ORM;
-use Symfony\Bridge\Doctrine\Types\UuidType;
-use Symfony\Component\Uid\Uuid;
-
-#[ORM\Entity(repositoryClass: DestinationRepository::class)]
 class Destination
 {
-    #[ORM\Id]
-    #[ORM\Column(type: UuidType::NAME, unique: true)]
-    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
-    #[ORM\CustomIdGenerator(class: 'doctrine.uuid_generator')]
-    private ?Uuid $id = null;
+    private int $destID;
+    private string $city;
+    private string $country;
+    private string $subtitle;
+    private array $destImages;
+    private int $reviewsCount;
+    private float $avgScore;
+    private string $description;
 
-    #[ORM\Column(length: 64)]
-    private ?string $city = null;
-
-    #[ORM\Column(length: 64)]
-    private ?string $country = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $subtitle = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?float $avg_rating = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $description = null;
-
-    /**
-     * @var Collection<int, Image>
-     */
-    #[ORM\OneToMany(targetEntity: Image::class, mappedBy: 'belongs_to', orphanRemoval: true)]
-    private Collection $images;
-
-    public function __construct()
-    {
-        $this->images = new ArrayCollection();
-    }
-
-    public function getId(): ?Uuid
-    {
-        return $this->id;
-    }
-
-    public function setId(Uuid $id): static
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(string $city): static
-    {
+    public function __construct(
+        int $destID,
+        string $city,
+        string $country,
+        string $subtitle,
+        array $destImages,
+        int $reviewsCount,
+        float $avgScore,
+        string $description
+    ) {
+        $this->destID = $destID;
         $this->city = $city;
-
-        return $this;
-    }
-
-    public function getCountry(): ?string
-    {
-        return $this->country;
-    }
-
-    public function setCountry(string $country): static
-    {
         $this->country = $country;
-
-        return $this;
-    }
-
-    public function getSubtitle(): ?string
-    {
-        return $this->subtitle;
-    }
-
-    public function setSubtitle(string $subtitle): static
-    {
         $this->subtitle = $subtitle;
-
-        return $this;
-    }
-
-    public function getAvgRating(): ?float
-    {
-        return $this->avg_rating;
-    }
-
-    public function setAvgRating(?float $avg_rating): static
-    {
-        $this->avg_rating = $avg_rating;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): static
-    {
+        $this->destImages = $destImages;
+        $this->reviewsCount = $reviewsCount;
+        $this->avgScore = $avgScore;
         $this->description = $description;
-
-        return $this;
     }
 
-    /**
-     * @return Collection<int, Image>
-     */
-    public function getImages(): Collection
-    {
-        return $this->images;
-    }
-
-    public function addImage(Image $image): static
-    {
-        if (!$this->images->contains($image)) {
-            $this->images->add($image);
-            $image->setBelongsTo($this);
-        }
-
-        return $this;
-    }
-
-    public function removeImage(Image $image): static
-    {
-        if ($this->images->removeElement($image)) {
-            // set the owning side to null (unless already changed)
-            if ($image->getBelongsTo() === $this) {
-                $image->setBelongsTo(null);
-            }
-        }
-
-        return $this;
-    }
+    // Getters for each property
+    public function getDestID(): int { return $this->destID; }
+    public function getCity(): string { return $this->city; }
+    public function getCountry(): string { return $this->country; }
+    public function getSubtitle(): string { return $this->subtitle; }
+    public function getDestImages(): array { return $this->destImages; }
+    public function getReviewsCount(): int { return $this->reviewsCount; }
+    public function getAvgScore(): float { return $this->avgScore; }
+    public function getDescription(): string { return $this->description; }
 }
